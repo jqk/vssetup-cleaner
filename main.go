ce384b447b26184dcb30949eac6e5d4bb0602839
Jason <service_register@126.com>
2023-01-13 09:02:47 +0800

    v1.0.0

--- main.go
+++ main.go
@@ -12,6 +12,13 @@ func main() {
 	packageListFile := flag.String("pkg", no_package_list_file, "package list file")
 	flag.Parse()
 
+	println("Visual Studio setup file cleaner 1.0.0\n")
+	println("  Arguments from command line or default value:")
+	println("    pkg  = ", *packageListFile)
+	println("    show = ", *showActionOnly)
+	println("    vs   = ", *setupPath)
+	println("\n---------------------------------------")
+
 	var result *CleanResult
 
 	if *packageListFile == no_package_list_file {
@@ -21,23 +28,30 @@ func main() {
 	}
 
 	if result.err != nil {
-		println("-----------------------------------")
-		println("Clean Visual Studio setup dir fail:\n", result.err.Error())
-		println("-----------------------------------")
-	} else {
 		println("---------------------------------------")
+		println("Clean Visual Studio setup dir fail:\n", result.err.Error())
+		println("---------------------------------------")
+	} else {
+		if result.backupCount > 0 {
+			println("\n---------------------------------------\n")
+		}
 		println("Clean Visual Studio setup dir finished.")
 		println("Visual Studio setup path is [", result.vsPath, "].")
 		println("Old version packages will be moved to [", result.backupPath, "].")
 
 		if result.backupCount == 0 {
-			println("Old version package is not found. Nothing to do.")
-		} else if *showActionOnly {
-			println(result.backupCount, "old version packages will be moved.")
+			println("Old version package is not found. Nothing to do.\n")
 		} else {
-			println(result.backupCount, "old version packages have been moved.")
-		}
+			packageWord := "packages"
+			if result.backupCount == 1 {
+				packageWord = "package"
+			}
 
-		println("---------------------------------------")
+			if *showActionOnly {
+				println(result.backupCount, "old version", packageWord, "will be moved.\n")
+			} else {
+				println(result.backupCount, "old version", packageWord, "have been moved.\n")
+			}
+		}
 	}
 }
